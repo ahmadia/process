0978be99263e301235a64c79502d28ecee37a0ee
Aron Ahmadia <aron@fen1.shaheen.kaust.edu.sa>
2010-08-13 15:57:11 +0300

    seriously revamped kslrun with tons of new features
    --HG--
    extra : convert_revision : 090a4da884bd07b0ac6da877364933dbec3bb510

---
+++ config/kslrun.py
@@ -0,0 +1,13 @@
+verbose            = False
+debug              = False
+no_std_redirect    = False
+keep_ll_file       = False
+no_notify          = False
+partition_size     = 128
+account            = 'k01'
+job_name           = 'kslrun_job'
+wall_time          = '5:00'
+ll_class           = 'default'
+prefix             = './$(job_name)_$(jobid)'
+np                 = 1
+mode               = 'SMP'
