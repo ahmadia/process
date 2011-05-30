f0be027eb7e0beeaa386c2caae90946c96180c40
Aron Ahmadia <aron@ahmadia.net>
2011-05-30 15:10:44 +0300

    requires distribute now [Python build tools suck]printenv

--- setup.py
+++ setup.py
@@ -1,29 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 import os
 
 def get_files(dir):
     return [os.path.join(dir,_file) for _file in os.listdir(dir) if os.path.isfile(os.path.join(dir,_file))]
 
 setup(name='process',
+      install_requires=['distribute']
       version='0.2.2',
       description='Automation tools for high performance computing environments',
       author='Aron Ahmadia',
       author_email='aron@ahmadia.net',
       url='http://github.com/ahmadia/process',
       packages=['ksl','ksl.process','ksl.process.install'],
+      include_package_data=True,
       scripts=['scripts/kslinstall','scripts/kslrun'],
-      data_files=[('examples',get_files('examples')),
-                  ('examples/patches',get_files('examples/patches')),
-                  ('system/config/ppc64',['system/config/ppc64/kslinstall.ini','system/config/ppc64/kslrun.ini']),
-                  ('system/config/x86_64',['system/config/x86_64/kslinstall.ini','system/config/x86_64/kslrun.ini']),
-                  ('system/modules/ppc64/compilers',get_files('system/modules/ppc64/compilers')),
-                  ('system/modules/x86_64/compilers',get_files('system/modules/x86_64/compilers')),
-                  ('system/modules/shared/tools',get_files('system/modules/shared/tools')),
-                  ('system/templates',['system/templates/module.ksl']),
-                  ('test/sanity',get_files('test/sanity')),
-                  ('test/system',get_files('test/system')),
-                  ('test',['test/__init__.py','test/test_sanity.py']),
-                  ]
      )
